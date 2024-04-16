9204c3d4cf4b671b3d8e2dde129f205282111c96
jonathanhamel4 <jonathanhamel4@gmail.com>
2024-04-16 15:25:32 -0400

    animate extension

--- assets/main.js
+++ assets/main.js
@@ -1,106 +1,206 @@
 // This script will be run within the webview itself
 // It cannot access the main VS Code APIs directly.
+const nodes = [];
+
 (function () {
-    const vscode = acquireVsCodeApi();
-    // const oldState = vscode.getState() || { colors: [] };
+    // const vscode = acquireVsCodeApi();
 
-    const template = document.querySelector("#drop");
-    const zone = document.querySelector('#zone');
+    const stopElement = document.querySelector('#stop');
+    stopElement.style.display = 'none';
+    stopElement.onclick = () => {
+        nodes.forEach((node) => {
+            node.stop();
+        });
+        nodes = [];
+    };
 
-    document.addEventListener('click', () => {
-        if (!template || !zone) { return; }
+    let sizeIntervalId = null;
+    let tempAnimatedNode = null;
+    document.addEventListener('mousedown', (e) => {
+        if (e.target.id === 'stop') { return; }
 
-        const copy = template.cloneNode(true).content;
-        zone.appendChild(copy.querySelector('.drop'));
-        vscode.postMessage({ type: 'clicked' });
+        tempAnimatedNode = new AnimatedNode(e.clientX, e.clientY, 5);
+        nodes.push(tempAnimatedNode);
+        sizeIntervalId = setInterval(() => {
+            tempAnimatedNode.incrementSize();
+        }, 200);
     });
 
-    // /** @type {Array<{ value: string }>} */
-    // let colors = oldState.colors;
+    document.addEventListener('mouseup', (e) => {
+        if (e.target.id === 'stop') { return; }
 
-    // updateColorList(colors);
-
-    // document.querySelector('.add-color-button').addEventListener('click', () => {
-    //     addColor();
-    // });
-
-    // // Handle messages sent from the extension to the webview
-    // window.addEventListener('message', event => {
-    //     const message = event.data; // The json data that the extension sent
-    //     switch (message.type) {
-    //         case 'addColor':
-    //             {
-    //                 addColor();
-    //                 break;
-    //             }
-    //         case 'clearColors':
-    //             {
-    //                 colors = [];
-    //                 updateColorList(colors);
-    //                 break;
-    //             }
-
-    //     }
-    // });
-
-    // /**
-    //  * @param {Array<{ value: string }>} colors
-    //  */
-    // function updateColorList(colors) {
-    //     const ul = document.querySelector('.color-list');
-    //     ul.textContent = '';
-    //     for (const color of colors) {
-    //         const li = document.createElement('li');
-    //         li.className = 'color-entry';
-
-    //         const colorPreview = document.createElement('div');
-    //         colorPreview.className = 'color-preview';
-    //         colorPreview.style.backgroundColor = `#${color.value}`;
-    //         colorPreview.addEventListener('click', () => {
-    //             onColorClicked(color.value);
-    //         });
-    //         li.appendChild(colorPreview);
-
-    //         const input = document.createElement('input');
-    //         input.className = 'color-input';
-    //         input.type = 'text';
-    //         input.value = color.value;
-    //         input.addEventListener('change', (e) => {
-    //             const value = e.target.value;
-    //             if (!value) {
-    //                 // Treat empty value as delete
-    //                 colors.splice(colors.indexOf(color), 1);
-    //             } else {
-    //                 color.value = value;
-    //             }
-    //             updateColorList(colors);
-    //         });
-    //         li.appendChild(input);
-
-    //         ul.appendChild(li);
-    //     }
-
-    //     // Update the saved state
-    //     vscode.setState({ colors: colors });
-    // }
-
-    // /** 
-    //  * @param {string} color 
-    //  */
-    // function onColorClicked(color) {
-    //     vscode.postMessage({ type: 'colorSelected', value: color });
-    // }
-
-    // /**
-    //  * @returns string
-    //  */
-    // function getNewCalicoColor() {
-    //     const colors = ['020202', 'f1eeee', 'a85b20', 'daab70', 'efcb99'];
-    //     return colors[Math.floor(Math.random() * colors.length)];
-    // }
-
-    // function addColor() {
-    //     colors.push({ value: getNewCalicoColor() });
-    //     updateColorList(colors);
-    // }
+        clearInterval(sizeIntervalId);
+        sizeIntervalId = null;
+        tempAnimatedNode.animate();
+        tempAnimatedNode = null;
+        size = 5;
+        stopElement.style.display = 'block';
+        // vscode.postMessage({ type: 'clicked' });
+    });
 }());
+
+class AnimatedNode {
+    constructor(x, y, size) {
+        // init
+        this.size = size;
+        this.clientWidth = window.innerWidth;
+        this.clientHeight = window.innerHeight;
+        const trueY = this.clientHeight - y;
+        this.x = x;
+        this.y = trueY;
+        this.node = this.createNode();
+        this.setBoundaries();
+
+        // slope info
+        this.a = 0;
+        this.b = 0;
+        this.intervalId = null;
+        this.direction = 'right';
+        this.node.style.backgroundColor = this.getRandomColor();
+
+        // add
+        this.appendHtmlNode();
+    }
+
+    /** returns a random color */
+    getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    /** Increments the size and centers the element with the cursor while the cursor is being held */
+    incrementSize() {
+        this.size += 2;
+        this.x -= 1;
+        this.y -= 1;
+
+        this.setBoundaries();
+    }
+
+    /** Sets the boundaries of the zone using the whole client dimensions */
+    setBoundaries() {
+        this.node.style.left = this.x + 'px';
+        this.node.style.bottom = this.y + 'px';
+        this.node.style.width = this.size + 'px';
+        this.node.style.height = this.size + 'px';
+
+        this.maxWidth = this.clientWidth - this.size;
+        this.maxHeight = this.clientHeight - this.size;
+        this.minHeight = this.size;
+        this.minWidth = this.size;
+    }
+
+    /** Creates the node that will be moving */
+    createNode() {
+        const template = document.querySelector("#drop");
+        const copy = template.cloneNode(true).content;
+        const node = copy.querySelector('.drop');
+
+        node.style.left = this.x + 'px';
+        node.style.bottom = this.y + 'px';
+        node.style.width = this.size + 'px';
+        node.style.height = this.size + 'px';
+        return node;
+    }
+
+    /** Adds the node to the html document */
+    appendHtmlNode() {
+        const zone = document.querySelector('#zone');
+        zone.appendChild(this.node);
+    }
+
+    /** Starts the animation of the element. It defines the initial slope based on the
+     * initial click and the (x,y) defined by the center of the right edge.
+     */
+    animate() {
+        const rightCenterY = this.clientHeight / 2;
+        const rightMostX = this.clientWidth;
+
+        // y = ax + b;
+        // initial slope (y2-y1)/(x2-x1)
+        this.a = (rightCenterY - this.y) / (rightMostX - this.x);
+        this.b = (this.a * this.x - this.y) * -1;
+        this.direction = 'right';
+        this.move();
+    }
+
+    /** Defines the movement and collision behaviours through an interval */
+    move() {
+        this.intervalId = setInterval(() => {
+            const { x: newX, y: newY } = this.increment();
+
+            if (this.hasCollision(newX, newY)) {
+                clearInterval(this.intervalId);
+                this.newSlopeAndDirection(newX, newY);
+                this.move();
+            }
+
+            this.x = newX;
+            this.y = newY;
+            this.node.style.bottom = this.y + 'px';
+            this.node.style.left = this.x + 'px';
+        }, 40);
+    }
+
+    /** Adjusts the slope and direction whenever there is a collision */
+    newSlopeAndDirection(tempX) {
+        const collisionWithSide = tempX <= this.minWidth || tempX >= this.maxWidth;
+        switch (this.direction) {
+            case 'right': {
+                if (collisionWithSide) {
+                    this.direction = 'left';
+                }
+                break;
+            }
+            case "left": {
+                if (collisionWithSide) {
+                    this.direction = 'right';
+                }
+                break;
+            }
+        }
+
+        this.a = this.a * - 1;
+        this.b = this.y - this.a * this.x;
+    }
+
+    /** Increments the x position to find a new (x, y) coordinate for the element */
+    increment() {
+        switch (this.direction) {
+            case 'right': {
+                let nextY = this.a * (this.x + 1) + this.b;
+                return { x: this.x + 1, y: nextY };
+            }
+            case "left": {
+                let nextY = this.a * (this.x - 1) + this.b;
+                return { x: this.x - 1, y: nextY };
+            }
+            default:
+                return { x: this.x, y: this.y };
+        }
+    }
+
+    /** Check if there has been a collision */
+    hasCollision(x, y) {
+        switch (this.direction) {
+            case 'right': {
+                return x >= this.maxWidth || y >= this.maxHeight || y <= this.minHeight;
+            }
+            case "left": {
+                return x <= this.minWidth || y >= this.maxHeight || y <= this.minHeight;
+            }
+            default:
+                return false;
+        }
+    }
+
+    /** Removes the node from the html and clears the interval */
+    stop() {
+        clearInterval(this.intervalId);
+        this.node.remove();
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "animate-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// // The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('animate-extension.helloWorld', () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage('Hello World from animate-extension!');
-	// });
-
-	// context.subscriptions.push(disposable);
-
 	const provider = new AnimateView(context.extensionUri);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(AnimateView.viewType, provider));
@@ -50,7 +30,6 @@ class AnimateView {
 		webviewView.webview.options = {
 			// Allow scripts in the webview
 			enableScripts: true,
-
 			localResourceRoots: [
 				this.extensionUri
 			]
@@ -87,6 +66,7 @@ class AnimateView {
 				<link href="${styleMainUri}" rel="stylesheet">
 			</head>
 			<body>
+				<button id="stop" type="button">Stop</button>
 				<div id="zone"><div>
 				<template id="drop"><span class="drop"></span></template>
 				<script nonce="${nonce}" src="${scriptUri}"></script>
